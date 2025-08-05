acecaf3e3ab392e914d4109cc64dab4867c8dd6c
joan purba <joanpurba562@gmail.com>
2025-08-05 15:23:24 +0700

    feat: create links page

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { lazy, StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-const Home = lazy(() => import("./pages/Home.tsx"));
+const Links = lazy(() => import("./pages/Links.tsx"));
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([{ path: "/", element: <Links /> }]);
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
